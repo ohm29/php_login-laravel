66db2003195a56809656191f33601571b4343aeb
Thanapol Savasdee <42561667+Ohm29thanapolsawasdee@users.noreply.github.com>
2019-12-25 15:37:49 +0700

    finish

---
+++ login.php
@@ -0,0 +1,41 @@
+<?php 
+session_start();
+        if(isset($_POST['Username'])){
+				//connection
+                  include("connection.php");
+				//รับค่า user & password
+                  $Username = $_POST['Username'];
+                  $Password = $_POST['Password'];
+				//query 
+                  $sql="SELECT * FROM User Where Username='".$Username."' and Password='".$Password."' ";
+
+                  $result = mysqli_query($con,$sql);
+				
+                  if(mysqli_num_rows($result)==1){
+
+                      $row = mysqli_fetch_array($result);
+
+                      $_SESSION["UserID"] = $row["ID"];
+                      $_SESSION["User"] = $row["Name"]." ".$row["Department"];
+                      $_SESSION["status"] = $row["status"];
+
+                    
+                        Header("Location: admin_page.php");
+
+                      }
+
+                      
+
+                  }else{
+                    echo "<script>";
+                        echo "alert(\" user หรือ  password ไม่ถูกต้อง\");"; 
+                        echo "window.history.back()";
+                    echo "</script>";
+                    Header("Location: form.php"); //user & password incorrect back to login again
+                  }
+
+      
+
+
+  
+?>
